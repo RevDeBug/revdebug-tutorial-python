1a9431ec31989e09b058ab386cc5d799d481e75a
Mateusz Kaszyk <mateuszkaszyk@gmail.com>
2021-01-21 15:05:14 +0100

    Change /invoice/ endpoint to /buildIvoice

--- main/urls.py
+++ main/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    	path('invoices/', include('invoices.urls')),
+    	path('buildInvoice', include('invoices.urls')),
 	path('admin/', admin.site.urls),
 ]
 
